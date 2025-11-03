fd4d14f64261d6fabd6dd0ae26091e5c8d19abed
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-11-04 00:26:13 +0530

    variables and dataType

---
+++ basics/dataType.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(3+3);
+
+let name = "Utkarsh"; //string => "Utkarsh"
+let age = 21;
+let isLoggedIn = false; //boolean => true/false
+let state; //undefined => no value assigned
+let nullVar = null; //null => empty value 
+
+//number 
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not given
+// symblo =
---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountID = 12345;
+let accountEmail ="utkarsh@gmail.com"
+var accountPassword = "Utkarsh@123"
+accountCity = "Udaipur"
+
+//accountId = 2
+
+//console.log(accountID);
+
+accountEmail = "abcd@gmail.com"
+console.log(accountEmail);
+
+accountPassword = "abcd@123"
+accountCity = "Delhi"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
