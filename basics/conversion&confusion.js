7ad3313b1b36163b5265d76b1dc27b6b5384b507
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-11-04 22:55:44 +0530

    Conversion of datatypes

---
+++ basics/conversion&confusion.js
@@ -0,0 +1,20 @@
+let score = "33abc";
+console.log(typeof score); //string
+console.log(typeof(score)); //string
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber)); //number> unique identifier> unique identifier
+//object => key value pairs
+console.log(valueInNumber); //number
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1=> true; 0=> false
+//""=> false; "Utkarsh"=> true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
