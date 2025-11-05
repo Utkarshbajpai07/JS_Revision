1c2c59bebc392d4b66d5ab854e45ae80cfb98bd6
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-11-05 11:37:22 +0530

    Conversion Operation

---
+++ basics/Operation.js
@@ -0,0 +1,12 @@
+// let value = 33
+// let negvalue = -value
+// console.log(value);
+// console.log(negvalue); 
+
+let str1 = "Hello"
+let str2 = " Utkarsh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(+true); // 1
+//console.log(true+); // Error
