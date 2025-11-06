06b17e6c5f63e15c8e84abed83a40401df28b42b
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-11-06 12:03:29 +0530

    strings

---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+const name ="Utkarsh"
+const repoCount = 50
+console.log(name + repoCount); // Utkarsh50
+
+console.log(`Hello ${name}, you have ${repoCount} repositories`); // Hello Utkarsh, you have 50 repositories
+
+const gamename = new String("Mario")
+console.log(gamename); // [String: 'Mario']
+
+console.log (gamename[2]);
+
+console.log(gamename.length); // 5
+
+console.log(gamename.slice(0,3)); // Mar
+
